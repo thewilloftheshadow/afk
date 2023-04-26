dd2c0cd819fcbcc7df17e065c188ee620ea54785
Shadow <hi@shadowing.dev>
2023-04-26 13:04:26 -0500

    boop

--- src/main/java/com/shadowdev/purpurafk/listeners/AFKListener.java
+++ src/main/java/com/shadowdev/purpurafk/listeners/AFKListener.java
@@ -1,4 +1,12 @@
+package com.shadowdev.purpurafk.listeners;
+
 import org.bukkit.event.Listener;
+import org.bukkit.metadata.MetadataValue;
+import org.purpurmc.purpur.event.PlayerAFKEvent;
+
+import com.shadowdev.purpurafk.PurpurAFK;
+
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 
@@ -9,5 +17,17 @@ public final class AFKListener implements Listener {
 
 	@EventHandler(priority = EventPriority.HIGHEST)
 	public void onPlayerAFKEvent(PlayerAFKEvent event) {
+		Player player = event.getPlayer();
 
+		boolean vanished = false;
+		for (MetadataValue metadataValue : player.getMetadata("vanished")) {
+			if (metadataValue.asBoolean()) {
+				vanished = true;
+				break;
+			}
+		}
+
+		if (vanished)
+			event.setCancelled(true);
+	}
 }
